e4f35f3a185f5fcf59990842a3a260bfa1c361ce
Emre Turgut <emre.turgut@trendyol.com>
2021-11-28 19:21:35 +0300

    initial commit

---
+++ src/main/java/com/emreturgutce/kafkabeginners/tutorial1/ProducerDemo.java
@@ -0,0 +1,8 @@
+package com.emreturgutce.kafkabeginners.tutorial1;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
