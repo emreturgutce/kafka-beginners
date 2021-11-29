e48795324829795df55ce26df1fffcf8017581b3
Emre Turgut <emre.turgut@trendyol.com>
2021-11-29 08:11:44 +0300

    producer

--- src/main/java/com/emreturgutce/kafkabeginners/tutorial1/ProducerDemo.java
+++ src/main/java/com/emreturgutce/kafkabeginners/tutorial1/ProducerDemo.java
@@ -1,8 +1,50 @@
 package com.emreturgutce.kafkabeginners.tutorial1;
 
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
 public class ProducerDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Logger logger = LoggerFactory.getLogger(ProducerDemo.class);
+
+        String bootstrapServers = "localhost:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world");
+
+        producer.send(record, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                if (e != null) {
+                    logger.error("Error while producing: ", e);
+                } else {
+                    logger.info("Received new metadata \n" +
+                            "Topic: " + recordMetadata.topic() + "\n" +
+                            "Partition: " + recordMetadata.partition() + "\n" +
+                            "Offset: " + recordMetadata.offset() + "\n" +
+                            "Timestamp: " + recordMetadata.timestamp());
+                }
+            }
+        });
+
+        producer.flush();
+        producer.close();
     }
 }
